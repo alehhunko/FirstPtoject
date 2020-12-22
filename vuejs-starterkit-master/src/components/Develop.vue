3dcbffb7306948ae6f8c9b0e4d98609ec247e044
Perevertkin MA <perevertkinma@gmail.com>
2020-12-22 22:06:26 +0300

    changed eslint rules

--- vuejs-starterkit-master/src/components/Develop.vue
+++ vuejs-starterkit-master/src/components/Develop.vue
@@ -1,41 +1,39 @@
 <template>
    <div class="develop">
       <h1>Простой калькулятор</h1>
-      <input type="number" v-model="first"></input>
-      <br></br>
+      <input type="number" v-model="first"/>
+      <br/>
       <select v-model="sign">
          <option>+</option>
          <option>-</option>
          <option>*</option>
          <option>/</option>
       </select>
-      <br></br>
-      <input type="number" v-model="second"></input>
-      <br></br>
+      <br/>
+      <input type="number" v-model="second"/>
+      <br/>
       <div><button @click="suma">РАВНО</button>={{culc}}</div>
    </div>
 </template>
 
 <script>
- /* eslint-disable indent, no-trailing-spaces, block-spacing, space-infix-ops, no-eval, semi, brace-style */ 
 export default {
-   name: 'develop',
-   data() {
-     return { 
-       first: 0, 
-       second: 0, 
-       sign: 0, 
-       culc: 0,
-       };
+  name: "develop",
+  data() {
+    return {
+      first: 0,
+      second: 0,
+      sign: 0,
+      culc: 0,
+    };
   },
   methods: {
-    suma() { 
-       if (this.sign===0) {
-          this.culc=0;
-          }
-       else {
-          this.culc=eval(this.first + this.sign + this.second);
-          }
+    suma() {
+      if (this.sign === 0) {
+        this.culc = 0;
+      } else {
+        this.culc = eval(this.first + this.sign + this.second);
+      }
     },
   },
 };
