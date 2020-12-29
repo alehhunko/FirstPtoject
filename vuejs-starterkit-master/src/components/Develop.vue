8eb49736e0a692676e0260f9ea07e17f44f3ef17
alehhunko <alehhunko@gmail.com>
2020-12-29 23:10:15 +0300

    style ant design

--- vuejs-starterkit-master/src/components/Develop.vue
+++ vuejs-starterkit-master/src/components/Develop.vue
@@ -1,18 +1,18 @@
 <template>
    <div class="develop">
       <h1>Простой калькулятор</h1>
-      <input type="number" v-model="first"></input>
+      <a-input style="width: 300px; background-color: gray" type="number" v-model="first"></a-input>
       <br></br>
-      <select v-model="sign">
-         <option>+</option>
-         <option>-</option>
-         <option>*</option>
-         <option>/</option>
-      </select>
+      <a-select v-model="sign">
+         <a-select-option value="+">+</a-select-option>
+         <a-select-option value="-">-</a-select-option>
+         <a-select-option value="*">*</a-select-option>
+         <a-select-option value="/">/</a-select-option>
+      </a-select>
       <br></br>
-      <input type="number" v-model="second"></input>
+      <a-input style="width: 300px; background-color: gray" type="number" v-model="second"></a-input>
       <br></br>
-      <div><button @click="suma">РАВНО</button>={{culc}}</div>
+      <div><a-button type="danger" @click="suma">РАВНО</a-button>={{culc}}</div>
    </div>
 </template>
 
@@ -24,7 +24,7 @@ export default {
      return { 
        first: 0, 
        second: 0, 
-       sign: 0, 
+       sign: '+', 
        culc: 0,
        };
   },
