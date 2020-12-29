5e3c106480c736057450753980e7810f5fb952f3
Perevertkin MA <perevertkinma@gmail.com>
2020-12-29 23:28:52 +0300

    fix errors

--- vuejs-starterkit-master/src/components/Develop.vue
+++ vuejs-starterkit-master/src/components/Develop.vue
@@ -1,31 +1,39 @@
 <template>
-   <div class="develop">
-      <h1>Простой калькулятор</h1>
-      <a-input style="width: 300px; background-color: gray" type="number" v-model="first"></a-input>
-      <br></br>
-      <a-select v-model="sign">
-         <a-select-option value="+">+</a-select-option>
-         <a-select-option value="-">-</a-select-option>
-         <a-select-option value="*">*</a-select-option>
-         <a-select-option value="/">/</a-select-option>
-      </a-select>
-      <br></br>
-      <a-input style="width: 300px; background-color: gray" type="number" v-model="second"></a-input>
-      <br></br>
-      <div><a-button type="danger" @click="suma">РАВНО</a-button>={{culc}}</div>
-   </div>
+  <div class="develop">
+    <h1>Простой калькулятор</h1>
+    <a-input
+      style="width: 300px; background-color: gray"
+      type="number"
+      v-model="first"
+    ></a-input>
+    <br/>
+    <a-select v-model="sign">
+      <a-select-option value="+">+</a-select-option>
+      <a-select-option value="-">-</a-select-option>
+      <a-select-option value="*">*</a-select-option>
+      <a-select-option value="/">/</a-select-option>
+    </a-select>
+    <br/>
+    <a-input
+      style="width: 300px; background-color: gray"
+      type="number"
+      v-model="second"
+    ></a-input>
+    <br />
+    <div><a-button type="danger" @click="suma">РАВНО</a-button>={{ culc }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-   name: 'develop',
-   data() {
-     return { 
-       first: 0, 
-       second: 0, 
-       sign: '+', 
-       culc: 0,
-       };
+  name: "develop",
+  data() {
+    return {
+      first: 0,
+      second: 0,
+      sign: "+",
+      culc: 0,
+    };
   },
   methods: {
     suma() {
