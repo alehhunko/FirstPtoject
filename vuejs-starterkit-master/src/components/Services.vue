0045588502f83af9e85377fc16ced03871455910
alehhunko <alehhunko@gmail.com>
2021-01-26 20:37:01 +0300

    use vuex

--- vuejs-starterkit-master/src/components/Services.vue
+++ vuejs-starterkit-master/src/components/Services.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="develop">
-  <div v-for="element in info" v-bind:key="element.id">{{ element.description }} : {{ element.rate }}</div>
-   </div>
+    <div v-for="element in info" v-bind:key="element.id">{{ element.description }} : {{ element.rate }}</div>
+    <br/>
+    <div v-for="post in categories" v-bind:key="post.id">{{ post.email }} : <br/>{{ post.body }}</div>
+  </div>
 </template>
 
 <script>
@@ -18,6 +20,12 @@ export default {
     axios 
       .get('https://api.coindesk.com/v1/bpi/currentprice.json')
       .then(response => (this.info = response.data.bpi));
+      this.$store.dispatch('getMail');
   },
+  computed: {
+    categories () {
+      return this.$store.state.all;
+    }
+  }
 };
 </script>
